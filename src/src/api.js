9ee390274e6d17628b0c67070e03d60c9a8880e7
roboaiapaths99 <roboaiapaths99@gmail.com>
2026-06-05 12:22:02 +0530

    fix: clean folder structure, remove nested duplicates

--- src/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const backendApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://logday-api.duckdns.org"
-});
-
-export const agentApi = axios.create({
-  baseURL: "http://127.0.0.1:7890"
-});
